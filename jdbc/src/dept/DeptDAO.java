732cddc22593d6fb6ba6af2ed4d7d58bf15a2cf5
jiyeon <alclsemfja@naver.com>
2021-09-30 15:58:08 +0900

    database 커넥션

--- jdbc/src/dept/DeptDAO.java
+++ jdbc/src/dept/DeptDAO.java
@@ -1,14 +1,32 @@
 package dept;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import com.sun.xml.internal.ws.Closeable;
+
 public class DeptDAO {
-	
+	//empdao static,Connection
 	
 	static {
 		try {
-			Class.forName("");
+			Class.forName("oracle.jdbc.OracleDriver");
 		} catch (ClassNotFoundException e) {			
 			e.printStackTrace();
 		}
 	}
-	
+	public static Connection getConnection() {
+		String url = "jdbc:oracle:thin:@125.132.75.187:1521:xe";
+		String user = "c##scott";
+		String password = "tiger";
+		Connection con = null;
+		try {
+			con= DriverManager.getConnection(url,user,password);
+		} catch (Exception e) {
+				e.printStackTrace();
+ 		}
+
+	return con;
+}
+
 }
