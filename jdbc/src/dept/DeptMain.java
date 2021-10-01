e143bef52be23914d1c46eaf29f2de7fb3e3fc5b
jiyeon <alclsemfja@naver.com>
2021-10-01 11:17:33 +0900

    메뉴화면 구성

--- jdbc/src/dept/DeptMain.java
+++ jdbc/src/dept/DeptMain.java
@@ -1,10 +1,19 @@
 package dept;
 
+import java.util.Scanner;
+
 public class DeptMain {
 
 	public static void main(String[] args) {
   
+		boolean run=true;
 		
+		Scanner sc = new Scanner(System.in);
+		
+		while () {
+			
+			
+		}
 		
 		
 	}
